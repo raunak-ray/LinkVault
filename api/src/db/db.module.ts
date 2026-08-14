da8becbd4b296496bbd6afed8e60e0f7f63cb1e2
Raunak Ray <raunakray3260@gmail.com>
2026-08-14 13:08:06 +0530

    feat(db): integrate drizzle ORM with PostgreSQL configuration

---
+++ api/src/db/db.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { DbProvider } from './db.provider';
+
+@Module({
+    providers: [DbProvider],
+    exports: [DbProvider]
+})
+export class DbModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger("main");
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT ?? 5050;
+  
+  await app.listen(PORT);
+  logger.log(`Server started on: http://localhost:${PORT}/`)
 }
 bootstrap();
